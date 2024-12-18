e36237ac30183d64a2e53d7755d297b9ab094244
neil-toscano <neil.toscano.f@uni.pe>
2024-12-18 17:28:34 -0500

    cambios-log-estado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    return await this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    try {
+      const dato = await axios.get('http://localhost:5005');
+      console.log('dato', dato);
+    } catch (error) {
+      console.log('error-app');
+    }
     return 'Hola, soy el chatbot de la MUNISJL';
   }
 }
