6564e4ea893c3750830e250bc2da202ab11d64e3
neil-toscano <neil.toscano.f@uni.pe>
2024-12-18 10:46:12 -0500

    cambios-global-prefix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+@UsePipes(new ValidationPipe({ transform: false }))
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: '', method: RequestMethod.GET }],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
